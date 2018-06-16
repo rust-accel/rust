651b282fad3056446306da555a4f9abddba2db23
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-06-16 15:48:00 +0000

    Enable nvptx64-nvidia-cuda target

---
+++ src/librustc_target/spec/nvptx64_nvidia_cuda.rs
@@ -0,0 +1,35 @@
+/// Copied from wasm32-unknown-unknown
+
+use super::{LinkerFlavor, Target, TargetOptions, PanicStrategy};
+
+pub fn target() -> Result<Target, String> {
+    let opts = TargetOptions {
+        dynamic_linking: true,
+        only_cdylib: true,
+
+        executables: false,
+
+        exe_suffix: ".ptx".to_string(),
+        dll_prefix: "".to_string(),
+        dll_suffix: ".ptx".to_string(),
+        linker_is_gnu: true,
+
+        panic_strategy: PanicStrategy::Abort,
+
+        .. Default::default()
+    };
+    Ok(Target {
+        llvm_target: "nvptx64-nvidia-cuda".to_string(),
+        target_endian: "little".to_string(),
+        target_pointer_width: "64".to_string(),
+        target_c_int_width: "32".to_string(),
+        target_env: "".to_string(),
+        target_os: "cuda".to_string(),
+        target_vendor: "nvidia".to_string(),
+        data_layout: "e-i64:64-v16:16-v32:32-n16:32:64".to_string(),
+        arch: "nvptx64".to_string(),
+        linker_flavor: LinkerFlavor::Gcc,
+        options: opts,
+    })
+}
+
